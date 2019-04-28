fab395d365614e7508746a741bb2b8218898c773
Banjer71 <davgio7@gmail.com>
2019-04-28 15:10:38 +0100

    Updates

---
+++ precache-manifest.121f60b298824e678dff633125f875f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd2fd5e73ac34e7f60a68ac6e75e7537",
+    "url": "/my-react-notes/static/media/logo.cd2fd5e7.svg"
+  },
+  {
+    "revision": "83e475d90bdd717c8db8",
+    "url": "/my-react-notes/static/js/runtime~main.e1d76d61.js"
+  },
+  {
+    "revision": "6ac8bf33a09b5829fdb6",
+    "url": "/my-react-notes/static/js/main.84f0b6d8.chunk.js"
+  },
+  {
+    "revision": "bbf7f360499be5fc0af6",
+    "url": "/my-react-notes/static/js/2.1de47be2.chunk.js"
+  },
+  {
+    "revision": "6ac8bf33a09b5829fdb6",
+    "url": "/my-react-notes/static/css/main.52ed0784.chunk.css"
+  },
+  {
+    "revision": "399415ad26dec6c6ad033ebda6be5d07",
+    "url": "/my-react-notes/index.html"
+  }
+];
