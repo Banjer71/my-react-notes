2b33bf02dc3f713efdcb52f3e1e4510331e3cd0e
Banjer71 <davgio7@gmail.com>
2019-10-05 22:31:25 +0100

    change link

--- src/components/Main.js
+++ src/components/Main.js
@@ -6,7 +6,7 @@ import User from './User';
 function Main() {
 	return (
 		<div className="main">
-			<Card github="https://github.com/Banjer71/my-react-notes">
+		<Card github="https://github.com/Banjer71/my-react-notes/blob/master/src/components/Button_change.js">
 				<h2>Counter</h2>
 				<ButtonChange className="btn" />
 			</Card>
@@ -16,8 +16,8 @@ function Main() {
 				<User firstName="Davide" lastName="Naccarati" />
 			</Card>
 
-			<Card github="https://www.davidenaccarati.com">
-				<h2>?</h2>
+			<Card github="">
+				<h2>Who's next....</h2>
 			</Card>
 		</div>
 	);
